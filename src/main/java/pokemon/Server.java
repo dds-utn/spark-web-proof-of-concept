1308052fbfa6659ce718bf7d12afc70c7040042a
Gaston Prieto <gastonprietoutn@gmail.com>
2016-08-13 11:41:57 -0300

    basico

--- src/main/java/pokemon/Server.java
+++ src/main/java/pokemon/Server.java
@@ -1,13 +1,17 @@
 package pokemon;
 
+import static spark.Spark.*;
+
 import pokemon.model.Usuario;
 import pokemon.repositories.UsuarioRepositorio;
 
 public class Server {
 	public static void main(String[] args) {
 		Bootstrap.init();
-		
+
 		Usuario usuario = UsuarioRepositorio.get().findAny();
-		System.out.println(usuario.toString());
+
+		get("/usuario", (req, res) -> usuario.getUsuario());
+
 	}
 }
